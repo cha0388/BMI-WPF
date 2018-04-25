7ab02d97fae0cf337652871d1d788b3fad69bc2e
DESKTOP-KQBVSJ0\charl <charlotte0388@gmail.com>
2018-04-26 00:46:29 +0800

    3,能算出來ㄌ

--- BMI WPF/BMI WPF/MainWindow.xaml.cs
+++ BMI WPF/BMI WPF/MainWindow.xaml.cs
@@ -18,11 +18,53 @@ namespace BMI_WPF
     /// <summary>
     /// MainWindow.xaml 的互動邏輯
     /// </summary>
+    /// 
+   
     public partial class MainWindow : Window
     {
+
+        public class Global
+        {
+            public static string height, weight;
+            public static float H, bmi, W;
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void TextBox1_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            Global.height = Console.ReadLine();
+            Global.H = (float)int.Parse(Global.height) / 100;
+        }
+
+        private void Delete1_Click(object sender, RoutedEventArgs e)
+        {
+            Box1.Clear();
+        }
+
+        private void TextBox2_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            Global.weight = Console.ReadLine();
+            Global.W =int.Parse(Global.weight);
+        }
+
+        private void Delete2_Click(object sender, RoutedEventArgs e)
+        {
+            Box2.Clear();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string height = Box1.Text;
+            string weight = Box2.Text;
+            float H = (float)int.Parse(height) / 100;
+            float bmi = ( int.Parse(weight) / (H * H));
+            Text1.Text = bmi.ToString() ;
+        }
+        
     }
 }
