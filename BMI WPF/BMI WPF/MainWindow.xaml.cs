41b68925d1dc10294f906bcffa546da11a6dcca1
m305-7\w401 <charlotte0388@gmail.com>
2018-04-26 17:05:14 +0800

    6,加註解

--- BMI WPF/BMI WPF/MainWindow.xaml.cs
+++ BMI WPF/BMI WPF/MainWindow.xaml.cs
@@ -22,50 +22,50 @@ namespace BMI_WPF
    
     public partial class MainWindow : Window
     {
-
+        //把身高、體重、bmi等會用到的數值設為全域變數
         public class Global
         {
             public static string height, weight;
             public static float H, bmi, W;
         }
 
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void TextBox1_TextInput(object sender, TextCompositionEventArgs e)
-        {
-            Global.height = Console.ReadLine();
-            Global.H = (float)int.Parse(Global.height) / 100;
-        }
-
+        //點擊Del1會觸發以下事件
         private void Delete1_Click(object sender, RoutedEventArgs e)
         {
+            //Del1背景色變色
             Del1.Background = Brushes.LemonChiffon;
+
+            //清除TextBox1的內容
             Box1.Clear();
         }
 
-        private void TextBox2_TextInput(object sender, TextCompositionEventArgs e)
-        {
-            Global.weight = Console.ReadLine();
-            Global.W =int.Parse(Global.weight);
-        }
-
+        //點擊Del2會觸發以下事件
         private void Delete2_Click(object sender, RoutedEventArgs e)
         {
+            //Del2背景色變色
+            Del2.Background = Brushes.LemonChiffon;
+            //清除TextBox2的內容
             Box2.Clear();
         }
 
+        //點擊B會觸發以下事件
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            B.Background = Brushes.Teal;
+            //導入Box1、Box2的文字並轉為數值，設為height跟weight
             string height = Box1.Text;
             string weight = Box2.Text;
+
+            //計算bmi
             float H = (float)int.Parse(height) / 100;
-            float bmi = ( int.Parse(weight) / (H * H));
-            Text1.Text = bmi.ToString() ;
+            float bmi = (int.Parse(weight) / (H * H));
+
+            //寫出bmi並取到小數點後第二位
+            Text1.Text = bmi.ToString("0.00") ;
         }
         
     }
